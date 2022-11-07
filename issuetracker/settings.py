6c7d2c26653e8cf035b4b45506438cfd639a7b33
Erich Malberg <emalberg@live.com>
2022-11-07 09:39:39 -0700

    Update settings.py

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qf63@p%ayt@a&o3$p53a#sc$)$hslf27q$2qh0npr8-da&5mil'
+#SECRET_KEY = 'django-insecure-qf63@p%ayt@a&o3$p53a#sc$)$hslf27q$2qh0npr8-da&5mil'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
